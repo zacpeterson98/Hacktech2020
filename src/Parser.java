43bf0636c75d6180dcfdc187c6ed50e1138b3d14
zacpeterson98 <54647160+zacpeterson98@users.noreply.github.com>
2020-03-07 11:11:21 -0800

    Update Parser.java
    Sample parser function to test dependencies. Added dependencies

---
+++ src/Parser.java
@@ -0,0 +1,40 @@
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+import java.math.BigDecimal;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import java.util.*;
+import com.spoonacular.client.ApiException;
+import java.math.BigDecimal;
+import java.io.File;
+import com.spoonacular.client.model.InlineObject8;
+
+
+public class Parser {
+    private final static com.spoonacular.DefaultApi api = new com.spoonacular.DefaultApi();
+    private final static String requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/find" +
+            "ByIngredients?number=5&ranking=1&ingredients=";
+
+
+    public static String getAllergen(String allergen, String ingredients) throws ApiException {
+        BigDecimal number = BigDecimal.ONE;
+        boolean limitLicense = true;
+        BigDecimal ranking = BigDecimal.ONE;
+        boolean ignorePantry = false;
+        String ingredientsString = ;
+
+        Object response = api.searchRecipesByIngredients(ingredients, number, limitLicense, ranking, ignorePantry);
+        
+
+        return response;
+    }
+
+
+}
